9300b6b8a4105c773e2ce73c461b481450c2ec3b
istruthy <daren.lunsford+istruthy@gmail.com>
2019-11-24 13:29:42 -0800

    styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 // import Header from "./header"
-
-import "./layout.css"
+// import "./layout.css"
 
 const Layout = ({ children }) => {
   return <React.Fragment>{children}</React.Fragment>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,7 +7,15 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h3>Top Ten</h3>
+    <ButtonWrapper>Click me</ButtonWrapper>
   </Layout>
 )
+
+const ButtonWrapper = styled.button`
+  background: blue;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+`
 
 export default IndexPage
