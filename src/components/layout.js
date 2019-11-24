fd60bc9b933ff7b50fef3ea0fb27008e14e3be37
istruthy <daren.lunsford+istruthy@gmail.com>
2019-11-24 13:16:34 -0800

    adds sass example to project

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 // import Header from "./header"
 
-import "./layout.css"
+// import "./layout.css"
+import "../sass/layout.scss"
 
 const Layout = ({ children }) => {
   return <React.Fragment>{children}</React.Fragment>
