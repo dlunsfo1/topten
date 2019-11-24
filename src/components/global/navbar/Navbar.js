a60f797066482a1bba889ff419c7c0a6a800505d
istruthy <daren.lunsford+istruthy@gmail.com>
2019-11-24 14:25:00 -0800

    adds navbarHeader setup

---
+++ src/components/global/navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+
+import NavbarHeader from "./NavbarHeader"
+import NavbarLinks from "./NavbarLinks"
+import NavbarIcons from "./NavbarIcons"
+
+import styled from "styled-components"
+
+export default class Navbar extends Component {
+  state = {
+    navbarOpen: false,
+  }
+
+  handleNavbar = () => {
+    this.setState(() => {
+      return { navbarOpen: !this.state.navbarOpen }
+    })
+  }
+
+  render() {
+    const { navbarOpen } = this.state
+    return (
+      <NavWrapper>
+        <NavbarHeader handleNavBar={this.handleNavbar} />
+        <NavbarLinks navbarOpen={navbarOpen} />
+        <NavbarIcons />
+      </NavWrapper>
+    )
+  }
+}
+
+const NavWrapper = styled.nav`
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import Navbar from "../components/global/navbar/"
+
 // import { useStaticQuery, graphql } from "gatsby"
 // import Header from "./header"
 // import "./layout.css"
 
 const Layout = ({ children }) => {
-  return <React.Fragment>{children}</React.Fragment>
+  return (
+    <React.Fragment>
+      <Navbar />
+      {children}
+    </React.Fragment>
+  )
 }
 
 Layout.propTypes = {
