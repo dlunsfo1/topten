f72695df7eeb1375d8bb5a12bd95077e525eb2b6
istruthy <daren.lunsford+istruthy@gmail.com>
2019-12-04 19:54:29 -0800

    [listing] set up initial listing page template

--- src/utils/Banner.js
+++ src/utils/Banner.js
@@ -1,16 +1,17 @@
-import React from "react"
-import styled from "styled-components"
-import { styles } from "../utils"
+import React from 'react';
+import styled from 'styled-components';
+import { styles } from '../utils';
 
-export const Banner = ({ title, subtitle, children }) => {
+export const Banner = ({ title, subtitle, type, children }) => {
   return (
     <BannerWrapper>
       <h1 className="title">{title}</h1>
       <h3 className="subtitle">{subtitle}</h3>
+      <h4 className="type">{type}</h4>
       {children}
     </BannerWrapper>
-  )
-}
+  );
+};
 
 const BannerWrapper = styled.div`
   margin-bottom: 3rem;
@@ -19,11 +20,11 @@ const BannerWrapper = styled.div`
     color: ${styles.colors.mainWhite};
     font-size: 2rem;
     text-transform: uppercase;
-    ${styles.letterSpacing({ spacing: "0.1rem" })}
+    ${styles.letterSpacing({ spacing: '0.1rem' })}
   }
   .subtitle {
     color: ${styles.colors.mainWhite};
     text-transform: uppercase;
-    ${styles.letterSpacing({ spacing: "0.1rem" })}
+    ${styles.letterSpacing({ spacing: '0.1rem' })}
   }
-`
+`;
